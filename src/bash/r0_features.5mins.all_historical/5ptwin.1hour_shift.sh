389df6db0b12eec1a09c3b7e398bce7f7e750541
James Montgomery <jamesmo@jpl.nasa.gov>
2021-04-14 16:54:08 -0700

    Updated configs

--- src/bash/r0_features.5mins.all_historical/5ptwin.1hour_shift.sh
+++ src/bash/r0_features.5mins.all_historical/5ptwin.1hour_shift.sh
@@ -3,16 +3,15 @@
 conda activate mloc
 
 mkdir /data1/mloc/local/runs/r0_features.5mins.all_historical
-mkdir /data1/mloc/local/runs/r0_features.5mins.all_historical/5ptwin.combined
+mkdir /data1/mloc/local/runs/r0_features.5mins.all_historical/5ptwin.1hour_shift
 
 cd /data1/mloc/src/
 
-exec 2>&1 | tee /data1/mloc/local/runs/r0_features.5mins.all_historical/5ptwin.combined/run.log
+exec 2>&1 | tee /data1/mloc/local/runs/r0_features.5mins.all_historical/5ptwin.1hour_shift/run.log
 
-python preprocess.py       -c configs/r0_features.5mins.all_historical/5ptwin.combined.yml -s preprocess
-python extract_features.py -c configs/r0_features.5mins.all_historical/5ptwin.combined.yml -s extract_features
-python classify.py         -c configs/r0_features.5mins.all_historical/5ptwin.combined.yml -s classify-1
-python classify.py         -c configs/r0_features.5mins.all_historical/5ptwin.combined.yml -s classify-2
-python classify.py         -c configs/r0_features.5mins.all_historical/5ptwin.combined.yml -s classify-3
+python preprocess.py       -c configs/r0_features.5mins.all_historical/5ptwin.1hour_shift.yml -s preprocess
+python extract_features.py -c configs/r0_features.5mins.all_historical/5ptwin.1hour_shift.yml -s extract_features
+python preprocess.py       -c configs/r0_features.5mins.all_historical/5ptwin.1hour_shift.yml -s preprocess_features
+python classify.py         -c configs/r0_features.5mins.all_historical/5ptwin.1hour_shift.yml -s classify
 
 echo 'DONE'
