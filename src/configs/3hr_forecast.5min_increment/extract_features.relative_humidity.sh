6daec016e0890ff0fbe6b06bbad0e12d5625371f
James Montgomery <jamesmo@jpl.nasa.gov>
2021-04-13 18:18:09 -0700

    Updated configs

---
+++ src/configs/3hr_forecast.5min_increment/extract_features.relative_humidity.sh
@@ -0,0 +1,43 @@
+
+#!/bin/bash
+
+echo 'STARTING PREPROCESS'
+
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-1
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-2
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-3
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-4
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-5
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-6
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-7
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-8
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-9
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-10
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-11
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-12
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-13
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-14
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-15
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-16
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-17
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-18
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-19
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-20
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-21
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-22
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-23
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-24
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-25
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-26
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-27
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-28
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-29
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-30
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-31
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-32
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-33
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-34
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-35
+python /data1/mloc/src/extract_features.py -c extract_features.relative_humidity.yml -s extract_features-36
+
+echo 'DONE'
--- src/configs/3hr_forecast.5min_increment/run_all.sh
+++ src/configs/3hr_forecast.5min_increment/run_all.sh
@@ -7,34 +7,34 @@ cd /data1/mloc/src/configs/3hr_forecast.5min_increment/
 echo 'STARTING temperature'
 
 ./preprocess.temperature.sh
-./feature_extraction.temperature.sh
+./extract_features.temperature.sh
 
 echo 'DONE'
 
 echo 'STARTING wind_speed'
 
 ./preprocess.wind_speed.sh
-./feature_extraction.wind_speed.sh
+./extract_features.wind_speed.sh
 
 echo 'DONE'
 
 echo 'STARTING relative_humidity'
 
 ./preprocess.relative_humidity.sh
-./feature_extraction.relative_humidity.sh
+./extract_features.relative_humidity.sh
 
 echo 'DONE'
 
 echo 'STARTING pressure'
 
 ./preprocess.pressure.sh
-./feature_extraction.pressure.sh
+./extract_features.pressure.sh
 
 echo 'DONE'
 
 echo 'STARTING log_Cn2_10T'
 
 ./preprocess.log_Cn2_10T.sh
-./feature_extraction.log_Cn2_10T.sh
+./extract_features.log_Cn2_10T.sh
 
 echo 'DONE'
