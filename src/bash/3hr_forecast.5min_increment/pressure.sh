f4fe117bd31def5067891b6547d9a435e906238e
James Montgomery <jamesmo@jpl.nasa.gov>
2021-04-20 12:01:54 -0700

    Updated bash scripts

--- src/bash/3hr_forecast.5min_increment/pressure.sh
+++ src/bash/3hr_forecast.5min_increment/pressure.sh
@@ -9,8 +9,8 @@ cd /data1/mloc/src/
 
 exec 2>&1 | tee /data1/mloc/local/runs/3hr_forecast.5min_increment/pressure/run.log
 
-# python preprocess.py       -c configs/3hr_forecast.5min_increment/pressure.yml -s preprocess
-# python extract_features.py -c configs/3hr_forecast.5min_increment/pressure.yml -s extract_features
+python preprocess.py       -c configs/3hr_forecast.5min_increment/pressure.yml -s preprocess
+python extract_features.py -c configs/3hr_forecast.5min_increment/pressure.yml -s extract_features
 python classify.py         -c configs/3hr_forecast.5min_increment/pressure.yml -s classify
 
 echo 'DONE'
