a8da8b858e15d0a2b2b52de957f7da384fe85087
James Montgomery <jamesmo@jpl.nasa.gov>
2021-04-29 15:18:30 -0700

    Updated configs

--- src/bash/3hr_forecast.5min_increment/plot.r0_cn2_weather.sh
+++ src/bash/3hr_forecast.5min_increment/plot.r0_cn2_weather.sh
@@ -7,14 +7,14 @@ mkdir /data1/mloc/local/runs/3hr_forecast.5min_increment/r0_cn2_weather
 
 cd /data1/mloc/src/
 
-mkdir /data1/mloc/local/runs/3hr_forecast.5min_increment/r0_cn2_weather/H5
+mkdir /data1/mloc/local/runs/3hr_forecast.5min_increment/r0_cn2_weather/r0_10T_H5_min
 python plots.py -c configs/3hr_forecast.5min_increment/plot.r0_cn2_weather.yml \
                 -s classify-5 \
                 -ki features/r0_10T/historical_5_min/test \
                 -ko forecasts/r0_10T/H5 \
                 -m /data1/mloc/local/runs/3hr_forecast.5min_increment/r0_cn2_weather/r0_10T_H5_min
 
-mkdir /data1/mloc/local/runs/3hr_forecast.5min_increment/r0_cn2_weather/H180
+mkdir /data1/mloc/local/runs/3hr_forecast.5min_increment/r0_cn2_weather/r0_10T_H180_min
 python plots.py -c configs/3hr_forecast.5min_increment/plot.r0_cn2_weather.yml \
                 -s classify-180 \
                 -ki features/r0_10T/historical_5_min/test \
