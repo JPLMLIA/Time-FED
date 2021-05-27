104a0c006c6c9da5c90afe349ad0a768cf96fde5
James Montgomery <jamesmo@jpl.nasa.gov>
2021-05-27 13:46:35 -0700

    Added support for excluding label from historical shifts

--- src/extract_features.py
+++ src/extract_features.py
@@ -194,9 +194,12 @@ def select(df, label, config):
             # Remove the static columns
             static = shift[config.static]
 
-            # Create a copy of the label column and drop the label
-            shift[f'{label}_H{length}'] = lbl
-            shift = shift.drop(columns=[label]+config.static)
+            if not config.exclude_label:
+                # Create a copy of the label column and drop the label
+                shift[f'{label}_H{length}'] = lbl
+                shift = shift.drop(columns=[label]+config.static)
+            else:
+                shift = shift.drop(columns=config.static)
 
             # Shift the index by the length amount in minutes, add label back in
             shift.index += pd.Timedelta(f'{length} min')
