3c407ebc39f197fffa211d2b4b1e52540e82a107
James Montgomery <jamesmo@jpl.nasa.gov>
2021-06-15 13:47:59 -0700

    Created configs for all r0 run cases; simplified config structures

---
+++ src/configs/method1/r0/bash/run_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /data1/mloc/src/configs/method1/r0/bash/
+
+./r0.cn2.weather.historical.sh
+./r0.cn2.weather.sh
+./r0.weather.historical.sh
+./r0.weather.sh
+
+echo 'FINISHED ALL'
