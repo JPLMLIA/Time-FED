0b275ce95b6f7a2b6bbda3fb20141edc5b916f13
James Montgomery <jamesmo@jpl.nasa.gov>
2021-07-02 11:33:34 -0700

    Updated configs

--- src/configs/method1/r0/bash/r0.weather.historical.sh
+++ src/configs/method1/r0/bash/r0.weather.historical.sh
@@ -8,8 +8,8 @@ mkdir /data1/mloc/local/runs/method1/r0/r0.weather.historical
 
 cd /data1/mloc/src/
 
-python preprocess.py       -c configs/method1/r0/r0.weather.historical.yml -s preprocess
-python extract_features.py -c configs/method1/r0/r0.weather.historical.yml -s extract_features
+# python preprocess.py       -c configs/method1/r0/r0.weather.historical.yml -s preprocess
+# python extract_features.py -c configs/method1/r0/r0.weather.historical.yml -s extract_features
 python classify.py         -c configs/method1/r0/r0.weather.historical.yml -s classify
 
 echo 'DONE'
--- src/configs/method1/run_all_weather_full.sh
+++ src/configs/method1/run_all_weather_full.sh
@@ -2,9 +2,9 @@
 
 cd /data1/mloc/src/configs/method1
 
-./temperature/full.sh
-./pressure/full.sh
-./relative_humidity/full.sh
-./wind_speed/full.sh
+./temperature/bash/full.sh
+./pressure/bash/full.sh
+./relative_humidity/bash/full.sh
+./wind_speed/bash/full.sh
 
 echo 'DONE ALL'
