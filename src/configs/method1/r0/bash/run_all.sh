c0468b79f9bdd0becfe2036a96fdcf04fb29f065
James Montgomery <jamesmo@jpl.nasa.gov>
2021-07-01 21:04:54 -0700

    Updated configs

--- src/configs/method1/r0/bash/run_all.sh
+++ src/configs/method1/r0/bash/run_all.sh
@@ -2,8 +2,8 @@
 
 cd /data1/mloc/src/configs/method1/r0/bash/
 
-./r0.cn2.weather.historical.sh
-./r0.cn2.weather.sh
+#./r0.cn2.weather.historical.sh
+./r0.cn2.weather.sh -se
 ./r0.weather.historical.sh
 ./r0.weather.sh
 
