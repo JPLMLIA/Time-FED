be625022372985442986999a82269d62d8d19312
James Montgomery <J.Montgomery@jpl.nasa.gov>
2025-04-16 10:46:20 -0700

    Added license

--- timefed/utils/evaluate.py
+++ timefed/utils/evaluate.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 23 16:32:15 2024
-
-@author: marchett
-"""
+# Copyright 2025, by the California Institute of Technology. ALL RIGHTS RESERVED.
+# United States Government Sponsorship acknowledged. Any commercial use must be
+# negotiated with the Office of Technology Transfer at the California Institute of
+# Technology.
+#
+# This software may be subject to U.S. export control laws. By accepting this software,
+# the user agrees to comply with all applicable U.S. export laws and regulations. User
+# has the responsibility to obtain export licenses, or other export authority as may be
+# required before exporting such information to foreign countries or providing access
+# to foreign persons.
 
 import os
 import logging
